b12dc8d2e515d9d9803b4da6d23630594a6a6160
Autymnwilliams <autymnwilliams2026@u.northwestern.edu>
2026-05-11 12:34:47 -0500

    feat: exp 6 use cyclical doy encoding

--- train.R
+++ train.R
@@ -7,27 +7,17 @@
 #   train_model(train, val) -> list with predictions, model_type, features
 #
 # Current model: simple linear regression (baseline)
-# Features used: doy_sin, doy_cos, T2M + full weather feature set
+# Features used: cyclical day-of-year + all Exp 3 weather features
 # =============================================================================
 train_model <- function(train, val) {
   TARGET <- "ALLSKY_SFC_SW_DWN"
   
   features <- c(
-    "doy_sin", "doy_cos", "month_sin", "month_cos",
-    "CLRSKY_SFC_SW_DWN", "lat", "lon",
-    "T2M", "I(T2M^2)", "RH2M", "I(RH2M^2)", "WS2M", "PRECTOTCORR", "temp_range",
-    "T2M_7d_avg", "RH2M_7d_avg", "WS2M_7d_avg", "PRECTOTCORR_7d_avg",
-    "CLRSKY_SFC_SW_DWN:RH2M", "CLRSKY_SFC_SW_DWN:PRECTOTCORR"
+    "doy_sin", "doy_cos", "T2M", "T2M_MAX", "T2M_MIN",
+    "RH2M", "WS2M", "PRECTOTCORR", "temp_range"
   )
   
-  formula <- as.formula(paste(
-    TARGET,
-    "~ doy_sin + doy_cos + month_sin + month_cos +",
-    "CLRSKY_SFC_SW_DWN + lat + lon +",
-    "T2M + I(T2M^2) + RH2M + I(RH2M^2) + WS2M + PRECTOTCORR + temp_range +",
-    "T2M_7d_avg + RH2M_7d_avg + WS2M_7d_avg + PRECTOTCORR_7d_avg +",
-    "CLRSKY_SFC_SW_DWN:RH2M + CLRSKY_SFC_SW_DWN:PRECTOTCORR"
-  ))
+  formula <- as.formula(paste(TARGET, "~", paste(features, collapse = " + ")))
   
   # --- Fit model ---
   model <- lm(formula, data = train)
